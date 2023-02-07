cd28370c01bbbfb0a4da2aff3ec60b903b2d621a
juanca <juankospina0530@gmail.com>
2023-02-07 15:41:11 -0500

    Include info about what each player is choosing

--- main.js
+++ main.js
@@ -16,14 +16,27 @@ function getPlayerAnswer() {
     } else { return 2; }
 }
 
+function numToRockPaperScissor(ans) {
+    if (ans == 0){
+        return "rock";
+    } else if (ans == 1) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
 function evalWinner(playerAnswer, computerAnswer) {
+    let strPlayerAns = numToRockPaperScissor(playerAnswer);
+    let strComputerAns = numToRockPaperScissor(computerAnswer)
+
     let outcome = WinnerMat[computerAnswer][playerAnswer];
     if (outcome == 1) {
         playerWon++;
-        return `Player won, it has ${playerWon} wins`;
+        return `Player won, ${strPlayerAns} beats ${strComputerAns}`;
     } else if (outcome == 2){
         computerWon++;
-        return `Computer won, it has ${computerWon} wins`;
+        return `Player won, ${strComputerAns} beats ${strPlayerAns}`;
     } else {
         return `It's a tie, the current standing is ${playerWon.toString() + "-" + computerWon.toString()}`;
     }
@@ -35,6 +48,15 @@ for (let i = 0; i < 5; i++) {
     let playerAns = getPlayerAnswer();
     let computerAns = getComputerAnswer();
     console.log(evalWinner(playerAns, computerAns));
+    if (i == 4) {
+        if (playerWon > computerWon){
+            console.log(`Player Wins! (${playerWon} to ${computerWon})`);
+        } else if (computerWon > playerWon){
+            console.log(`Computer Wins! (${computerWon} to ${playerWon})`);
+        } else {
+            console.log(`It's a tie (${playerWon} to ${computerWon})`)
+        }
+    }
 }
 
 
